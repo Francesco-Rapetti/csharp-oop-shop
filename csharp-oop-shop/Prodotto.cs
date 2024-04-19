ad3b0d87be2f4dff506d75f09dd2f58113e8fab8
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-19 13:43:05 +0200

    Add getFullPrice method

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -36,6 +36,11 @@ namespace csharp_oop_shop
         {
             this.code = Random.Shared.Next(1, 100000000);
         }
+
+        public double getFullPrice()
+        {
+            return this.price * (1 + (double)this.iva / 100);
+        }
     }
 
     public enum Iva
